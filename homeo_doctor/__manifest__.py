e54ffcfa59baaba47f6285eb28d1f8fbfb0b3c27
Libiya <libiyasebastianhiworth@gmail.com>
2023-11-13 15:34:00 +0530

    first commit

---
+++ homeo_doctor/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Homeo_Doctor',
+    'version': '1.0',
+    'category': 'Sales/Sales',
+    'summary': 'Hospital Management Software',
+
+    'depends': [],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
