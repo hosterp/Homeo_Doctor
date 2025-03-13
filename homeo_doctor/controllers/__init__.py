84c278775057e90bf3251df6ec5e9482b444ea78
Libiya <libiyasebastianhiworth@gmail.com>
2025-03-13 12:47:24 +0530

    google recapche

---
+++ homeo_doctor/controllers/__init__.py
@@ -0,0 +1,22 @@
+from . import main
+
+import requests
+from odoo import http
+from odoo.http import request
+
+class AuthLogin(http.Controller):
+    @http.route('/web/login', type='http', auth="public", website=True)
+    def web_login(self, redirect=None, **kw):
+        recaptcha_response = kw.get('g-recaptcha-response')
+        secret_key = "6LfqC_MqAAAAAO4UIYViFc8aN-eq6gyja6eMsdJp"
+
+        # Verify reCAPTCHA
+        response = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            data={'secret': secret_key, 'response': recaptcha_response}
+        ).json()
+
+        if not response.get("success"):
+            return request.render('web.login', {'error': 'Invalid reCAPTCHA. Try again.'})
+
+        return http.redirect_with_hash('/web')
