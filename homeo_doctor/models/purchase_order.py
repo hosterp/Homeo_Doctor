8bb3236777fd150e484990a1ae76c9597bf35d52
Libiya <libiyasebastianhiworth@gmail.com>
2025-03-25 12:37:54 +0530

    purchase order page unwated button hide

--- homeo_doctor/models/purchase_order.py
+++ homeo_doctor/models/purchase_order.py
@@ -12,3 +12,20 @@ class PurchaseOrderInherit(models.Model):
         required=True,
         default=lambda self: self.env["res.partner"].search([], limit=1).id,
     )
+    store_in_charge=fields.Many2one('store.incharge',string='Store in charge')
+    approved_by=fields.Many2one('approved.store.person',string='Approved By')
+
+
+class ApprovedPerson(models.Model):
+    _name='approved.store.person'
+    _rec_name='approved_by'
+
+    approved_by=fields.Char(string='Approved By')
+
+
+class StoreInCharge(models.Model):
+    _name='store.incharge'
+    _rec_name='store_incharge'
+
+
+    store_incharge= fields.Char(string='Store In Charge')
