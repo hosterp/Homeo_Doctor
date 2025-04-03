30072054579f440b9ba0c0cded99b198bb4d3e92
Ashik <ashikrajan7@gmail.com>
2025-04-03 15:38:44 +0530

    style changed

--- homeo_doctor/models/general_billing.py
+++ homeo_doctor/models/general_billing.py
@@ -1,7 +1,7 @@
 from odoo import models, fields, api
 from datetime import datetime
 
-from odoo.odoo.exceptions import UserError
+# from odoo.odoo.exceptions import UserError
 
 
 class GeneralBilling(models.Model):
