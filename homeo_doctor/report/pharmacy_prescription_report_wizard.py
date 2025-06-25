934fb39e2db840fa8b4b94945f0dde165031c52f
Libiya <libiyasebastianhiworth@gmail.com>
2025-06-25 14:42:28 +0530

    sale report date wise

--- homeo_doctor/report/pharmacy_prescription_report_wizard.py
+++ homeo_doctor/report/pharmacy_prescription_report_wizard.py
@@ -7,22 +7,33 @@ class PharmacyPrescriptionReportWizard(models.TransientModel):
     date_from = fields.Date(string='Date From', required=True)
     date_to = fields.Date(string='Date To', required=True)
     medicine_id = fields.Many2one('product.product', string='Medicine')
+    with_patient_name = fields.Boolean(string="Include Patient Name", default=False)
 
+    # def print_report(self):
+    #     self.ensure_one()
+    #
+    #     domain = [
+    #         ('pharmacy_id.date', '>=', self.date_from),
+    #         ('pharmacy_id.date', '<=', self.date_to),
+    #     ]
+    #     if self.medicine_id:
+    #         domain.append(('product_id', '=', self.medicine_id.id))
+    #
+    #
+    #     lines = self.env['pharmacy.prescription.line'].search(domain)
+    #
+    #     return self.env.ref('homeo_doctor.report_pharmacy_prescription').report_action(lines)
     def print_report(self):
         self.ensure_one()
+        return self.env.ref('homeo_doctor.report_pharmacy_prescription').report_action(self)
 
+    def _get_prescription_lines(self):
         domain = [
             ('pharmacy_id.date', '>=', self.date_from),
             ('pharmacy_id.date', '<=', self.date_to),
         ]
         if self.medicine_id:
-            domain.append(('product_id', '=', self.medicine_id.id))  # Use correct field name
-
-        # Make sure this is correct model: pharmacy.prescription.line
-        lines = self.env['pharmacy.prescription.line'].search(domain)
-
-        # ✅ Pass actual line records
-        return self.env.ref('homeo_doctor.report_pharmacy_prescription').report_action(lines)
-
+            domain.append(('product_id', '=', self.medicine_id.id))
+        return self.env['pharmacy.prescription.line'].search(domain)
 
 
