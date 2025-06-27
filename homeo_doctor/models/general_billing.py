33efce2c93ead36c90439586c085a64464688281
Libiya <libiyasebastianhiworth@gmail.com>
2025-06-27 11:51:18 +0530

    general billing print style changed

--- homeo_doctor/models/general_billing.py
+++ homeo_doctor/models/general_billing.py
@@ -247,16 +247,7 @@ class GeneralBilling(models.Model):
             raise ValidationError("Please enter both staff name and password.")
         for record in self:
             record.status = 'paid'
-        return {
-            'type': 'ir.actions.client',
-            'tag': 'display_notification',
-            'params': {
-                'title': 'Payment Confirmed',
-                'message': f'Payment has been confirmed for {self.patient_name}',
-                'sticky': False,
-                'next': {'type': 'ir.actions.act_window_close'},
-            }
-        }
+        return self.env.ref('homeo_doctor.report_general_bill_report').report_action(self)
 
     @api.depends('general_bill_line_ids.quantity', 'general_bill_line_ids.total_amt', 'general_bill_line_ids.tax','rent')
     def _compute_totals(self):
