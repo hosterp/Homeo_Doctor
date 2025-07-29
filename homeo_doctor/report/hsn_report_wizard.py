6215c63147e9b43cd2b8f551079bb9e081330b16
Libiya <libiyasebastianhiworth@gmail.com>
2025-07-29 15:03:17 +0530

    sales hsn wise report header added

--- homeo_doctor/report/hsn_report_wizard.py
+++ homeo_doctor/report/hsn_report_wizard.py
@@ -4,8 +4,8 @@ class HSNReportWizard(models.TransientModel):
     _name = 'hsn.report.wizard'
     _description = 'HSN Report Wizard'
 
-    from_date = fields.Date(string="From Date", required=True)
-    to_date = fields.Date(string="To Date", required=True)
+    from_date = fields.Date(string="From Date", required=True,default=fields.Date.today)
+    to_date = fields.Date(string="To Date", required=True,default=fields.Date.today)
 
     def print_report(self):
         return self.env.ref('homeo_doctor.action_report_hsn_gst_summary').report_action(self)
